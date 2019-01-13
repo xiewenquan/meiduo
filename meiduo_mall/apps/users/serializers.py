bddd04c2d0adb36ad3d105c7984c98519feb8779
xiewenquan <15046789540@163.com>
2019-01-13 19:45:59 +0800

    同步发送激活邮件

--- meiduo_mall/apps/users/serializers.py
+++ meiduo_mall/apps/users/serializers.py
@@ -140,20 +140,20 @@ class UserEmailInfoSerializer(serializers.ModelSerializer):
         # recipient_list,   收件人列表
         recipient_list = [email]
 
-        # user_id = 8
+        # # user_id = 8
         verify_url = generic_verify_url(instance.id)
 
-        # html_message = '<p>尊敬的用户您好！</p>' \
-        #                '<p>感谢您使用美多商城。</p>' \
-        #                '<p>您的邮箱为：%s 。请点击此链接激活您的邮箱：</p>' \
-        #                '<p><a href="%s">%s<a></p>' % (email, verify_url, verify_url)
-        #
-        #
-        # send_mail(subject=subject,
-        #           message=message,
-        #           from_email=from_email,
-        #           recipient_list=recipient_list,
-        #           html_message=html_message)
+        html_message = '<p>尊敬的用户您好！</p>' \
+                       '<p>感谢您使用美多商城。</p>' \
+                       '<p>您的邮箱为：%s 。请点击此链接激活您的邮箱：</p>' \
+                       '<p><a href="%s">%s<a></p>' % (email, verify_url, verify_url)
+
+
+        send_mail(subject=subject,
+                  message=message,
+                  from_email=from_email,
+                  recipient_list=recipient_list,
+                  html_message=html_message)
 
         from celery_tasks.mail.tasks import send_celery_email
         send_celery_email.delay(subject,
