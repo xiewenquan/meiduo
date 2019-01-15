0e08b66099d4021c0c028b405aa3a645ca2f3d53
xiewenquan <15046789540@163.com>
2019-01-15 17:32:37 +0800

    自定义文件存储类的优化

--- meiduo_mall/meiduo_mall/settings.py
+++ meiduo_mall/meiduo_mall/settings.py
@@ -282,3 +282,7 @@ CKEDITOR_UPLOAD_PATH = ''  # 上传图片保存路径，使用了FastDFS，所�
 
 # django文件存储
 DEFAULT_FILE_STORAGE = 'utils.fastdfs.storage.MyStorage'
+
+# FastDFS
+FDFS_URL = 'http://192.168.100.128:8888/'  # 访问图片的路径域名 ip地址修改为自己机器的ip地址
+FDFS_CLIENT_CONF = os.path.join(BASE_DIR, 'utils/fastdfs/client.conf')
--- meiduo_mall/utils/fastdfs/storage.py
+++ meiduo_mall/utils/fastdfs/storage.py
@@ -17,7 +17,14 @@ class MyStorage(Storage):
     # def __init__(self, option=None):
     #     if not option:
     #         option = settings.CUSTOM_STORAGE_OPTIONS
-    #     ...
+    def __init__(self, config_path=None,config_url=None):
+        if not config_path:
+            fdfs_config = settings.FDFS_CLIENT_CONF
+            self.fdfs_config = fdfs_config
+        if not config_url:
+            fdfs_url = settings.FDFS_URL
+            self.fdfs_url = fdfs_url
+
     #3. 您的存储类必须实现_open()和_save() 方法,
     #   以及适用于您的存储类的任何其他方法。
     #因为我们的 Fdfs 是通过http来获取图片的,所以不需要打开方法
@@ -27,7 +34,9 @@ class MyStorage(Storage):
 
     def _save(self, name, content, max_length=None):
         #1. 创建Fdfs的客户端，让客户端加载配置文件
-        client=Fdfs_client('utils/fastdfs/client.conf')
+        # client=Fdfs_client('utils/fastdfs/client.conf')
+        # client = Fdfs_client(settings.FDFS_CLIENT_CONF)
+        client = Fdfs_client(self.fdfs_config)
         #2. 获取上传的文件
         file_data=content.read()
         #3. 上传图片，并获取　返回内容
@@ -44,4 +53,6 @@ class MyStorage(Storage):
 
     def url(self, name):
 
-        return 'http:192.168.100.128:8888/'+name
+        # return 'http:192.168.100.128:8888/'+name
+        # return  settings.FDFS_URL + name
+        return  self.fdfs_url + name
